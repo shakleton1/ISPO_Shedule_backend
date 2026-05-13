85e49b02935f4e7c11f05f966c2a69e56fc0244c
shackleton1 <robixer228@gmail.com>
2026-05-13 18:20:21 +0300

    Write PLX teaching flags explicitly

--- internal/httpapi/handlers_import_plx.go
+++ internal/httpapi/handlers_import_plx.go
@@ -808,15 +808,18 @@ func importPLXCurriculum(repo *schedule.Repository, parsed *plxParsedCurriculum,
 			calendarID = &cal.ID
 			for _, week := range parsed.CalendarWeeks {
 				week.CalendarID = cal.ID
-				if err := tx.Select(
-					"CalendarID",
-					"WeekNumber",
-					"WeekStartDate",
-					"ActivityCode",
-					"ActivityName",
-					"IsTeaching",
-					"Comment",
-				).Create(&week).Error; err != nil {
+				if err := tx.Exec(`
+INSERT INTO academic_calendar_weeks
+  (calendar_id, week_number, week_start_date, activity_code, activity_name, is_teaching, comment)
+VALUES (?, ?, ?, ?, ?, ?, ?)`,
+					week.CalendarID,
+					week.WeekNumber,
+					dateOnlyHTTP(week.WeekStartDate),
+					week.ActivityCode,
+					week.ActivityName,
+					week.IsTeaching,
+					week.Comment,
+				).Error; err != nil {
 					return fmt.Errorf("calendar week %d: %w", week.WeekNumber, err)
 				}
 				result.CalendarWeeks++
