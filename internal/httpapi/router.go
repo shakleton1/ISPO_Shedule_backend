c187200d7b6e4dbe3817fd9b6bb31846193112b0
shackleton1 <robixer228@gmail.com>
2026-05-14 18:49:05 +0300

    translate swagger docs to russian

--- internal/httpapi/router.go
+++ internal/httpapi/router.go
@@ -105,7 +105,7 @@ func NewRouter(deps RouterDeps) http.Handler {
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>ISPO Smart Schedule API Docs</title>
+		<title>Документация API расписания ИСПО</title>
 		<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
 		<style>
 			body { margin: 0; }
